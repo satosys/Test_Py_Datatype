945be7261e5696ebeecde97ef6f38d5f38b96a84
satosys <satya9pinu@gmail.com>
2018-04-06 21:38:31 -0400

    String tutorial Print formating

--- Test_dt.py
+++ Test_dt.py
@@ -4,3 +4,10 @@ sys.argv[0] ='first'
 sys.argv[1] ='first'
 sys.argv[2] ='first'
 sys.argv[3] ='first'
+
+from sys import argv
+
+sys.argv[0] ='first'
+sys.argv[1] ='first'
+sys.argv[2] ='first'
+sys.argv[3] ='first'
